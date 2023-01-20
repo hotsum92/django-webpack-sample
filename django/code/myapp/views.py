113a913a89b604317f69f51c3ed4bdeb2e6ab2ac
hotsuma kawashima <hkawashimajob1992@gmail.com>
2023-01-20 16:46:00 +0900

    arrayの追加

--- django/code/myapp/views.py
+++ django/code/myapp/views.py
@@ -4,6 +4,7 @@ import json
 def index_a(request):
     data = {
         'app': 'A',
+        'array': [1, 2, 3],
     }
     jsonData = json.dumps(data)
     return render(request, 'a.html', {'data': jsonData})
@@ -11,6 +12,7 @@ def index_a(request):
 def index_b(request):
     data = {
         'app': 'B',
+        'array': [1, 2, 3],
     }
     jsonData = json.dumps(data)
     return render(request, 'b.html', {'data': jsonData})
